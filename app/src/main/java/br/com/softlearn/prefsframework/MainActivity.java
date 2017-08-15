28ed9e14bf9c17301df475b6d115d6f90a776f23
Riser876 <senhas_gustavomalvestiti@hotmail.com>
2017-08-14 21:45:50 -0300

    projeto do menu em Android

---
+++ app/src/main/java/br/com/softlearn/prefsframework/MainActivity.java
@@ -0,0 +1,36 @@
+package br.com.softlearn.prefsframework;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==R.id.mnuConfig){
+            Intent intent = new Intent(this,ConfigActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
